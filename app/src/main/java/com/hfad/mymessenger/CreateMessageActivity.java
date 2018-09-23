62880973bf0c3c9827b3d836859b26e964675124
sebaek <sebaek@gmail.com>
2018-09-24 02:42:10 +0900

    init commit

---
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,17 @@
+package com.hfad.mymessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+    }
+}
