89738c702fe7edfd1262f986d31321bff41c7596
sebaek <sebaek@gmail.com>
2018-10-01 22:50:01 +0900

    create ReceiveMessageActivity and layout

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -1,7 +1,9 @@
 package com.hfad.mymessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class CreateMessageActivity extends Activity {
@@ -13,5 +15,6 @@ public class CreateMessageActivity extends Activity {
     }
 
     public void onSendMessage(View view) {
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
     }
 }
